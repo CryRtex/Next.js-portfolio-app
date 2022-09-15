aa23571211c78d4932b8f6697ba861fd535cc858
CryRtex <53979758+CryRtex@users.noreply.github.com>
2022-09-15 14:51:21 +0300

    Add

---
+++ pages/resume.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Head from 'next/head';
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+
+const resume = () => {
+  return (
+    <>
+      <Head>
+        <title>CryRtex | Resume</title>
+        <meta
+          name='description'
+          content='I’m a front-end web developer specializing in building (and occasionally designing) exceptional digital experiences.'
+        />
+        <link rel='icon' href='/fav.png' />
+      </Head>
+</>
+  );
+};
+
+export default resume;
